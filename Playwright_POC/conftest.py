b0d0c2df7d7a58f65298cad11bc5cbef6fd547de
boomak <booma1799@gmail.com>
2025-03-10 16:37:12 +0530

    Generate trace files and group it

--- Playwright_POC/conftest.py
+++ Playwright_POC/conftest.py
@@ -19,6 +19,16 @@ def load_environment_config(env):
         config = yaml.safe_load(f)
     return config.get(env)
 
+@pytest.fixture(scope= "session",autouse=True)
+def cleanup_traces():
+    trace_dir = "Traces"
+    if os.path.exists(trace_dir):
+        try:
+            shutil.rmtree(trace_dir)
+        except FileNotFoundError:
+            pass
+    yield
+
 # Fixture to handle the environment configuration
 @pytest.fixture(scope="session")
 def environment(request):
@@ -38,16 +48,39 @@ def pytest_runtest_protocol(item, nextitem):
 # qa dev stage (load those file values like qa url)
 @pytest.fixture(scope="function")
 def playwright_browser_context(playwright : Playwright,request,environment):
+    group_marker = None
+    for marker in request.node.iter_markers():
+        group_marker = marker.name
+        break
+    else:
+        group_marker = 'untagged'
+
+    if group_marker:
+        trace_dir = os.path.join("Traces", group_marker)
+
+    if not os.path.exists(trace_dir):
+        os.makedirs(trace_dir)
+
+    test_id = request.node.nodeid.replace("::", "_")
+    trace_file_path = os.path.join(trace_dir, f"{test_id}.zip")
     browserName = request.config.getoption("--browser-name")
     headlessMode = request.config.getoption("--headed-browser")
     browser = playwright.chromium.launch( channel=browserName,headless=headlessMode)
     context = browser.new_context()
+    context.tracing.start(screenshots=True, snapshots=True, sources=True)
     page =  context.new_page()
     #url = request.config.getoption("--url")
     url = environment['url']
     page.goto(url)
     # https://rahulshettyacademy.com/AutomationPractice/
     yield page
+    # if request.node.rep_call.failed:
+    #     context.tracing.stop(path=trace_file_path)
+    #     allure.attach.file(trace_file_path,name="Playwright Trace", attachment_type=allure.attachment_type.ZIP)
+    # else:
+    #     context.tracing.stop()  # Stop tracing without saving
+    context.tracing.stop(path=trace_file_path)
+    # allure.attach.file(trace_file_path, name="Playwright Trace", extension="ZIP")
     page.close()
     browser.close()
 
